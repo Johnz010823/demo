ee70359ffecbf64267201a368c9ee656782619b7
Johnz010823 <johnpaolosilvano24@gmail.com>
2025-11-25 00:27:42 +0800

    Fix static files

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'portfolio' / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (uploaded images, optional)
